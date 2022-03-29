8c09421ceaae7c3c62bb441d0a118ea907373c25
Saleh Ahmed <sahmed@GENETEC.COM>
2022-03-29 16:27:22 -0400

    add first test

--- Learning.API.Testing.RestSharp/Learning.API.Testing.RestSharp/UnitTest1.cs
+++ Learning.API.Testing.RestSharp/Learning.API.Testing.RestSharp/UnitTest1.cs
@@ -1,12 +1,44 @@
+using System;
+using System.Net.Security;
+using System.Threading.Tasks;
+using FluentAssertions;
+using RestSharp;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Learning.API.Testing.RestSharp
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
         {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            _testOutputHelper.WriteLine("First Test");
+
+            var options = new RestClientOptions
+            {
+                BaseUrl = new Uri("https://localhost:44330/"),
+                RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => true
+            };
+
+            // Rest Client
+            var client = new RestClient(options);
+
+            // Rest Request
+            var request = new RestRequest("/Product/GetProductById/1");
+
+            // Perform the Get Operation
+            var response = await client.GetAsync(request);
+
+            // Assertion
+            response.Should().NotBeNull();
 
         }
     }
